30f51356e47c137d6b9e0feeecc5bfa4d9188357
Vadym Myrgorod <dealancer@gmail.com>
2020-02-24 02:11:13 -0500

    Implement merge sort

--- demo.js
+++ demo.js
@@ -1,13 +1,43 @@
 const fl = require('./flockleader.js');
 
-const func = async function(i) {
-  if (i > 4)
-    return i;
-  return await run(i + 1);
+const mergeSort = async function(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  var [a, b] = await Promise.all([
+    run(arr.slice(0, Math.floor(arr.length / 2))).catch(reason => console.log(reason)),
+    run(arr.slice(Math.floor(arr.length / 2), arr.length)).catch(reason => console.log(reason)),
+  ]);
+
+  var resArr = [];
+  var i = 0, j = 0;
+  while (true) {
+    if ( i < a.length && j < b.length) {
+      if (a[i] < b[j]) {
+        resArr.push(a[i]);
+        i++;
+      } else {
+        resArr.push(b[j]);
+        j++;
+      }
+    } else if (i < a.length) {
+      resArr.push(...a.slice(i, a.length))
+      break;
+    } else if (j < b.length) {
+      resArr.push(...b.slice(j, b.length))
+      break;
+    }
+  }
+
+  return resArr;
 }
 
-fl.run(func, [1]).then(
-  result => console.log('result: ' + result)
+const arr = [9, 2, 4, 1, 0, -2, 4, 1, 4, 2, 8, 4];
+
+// TODO: figure out why no error messages are shown if there are error in mergeSort func.
+fl.run(mergeSort, [arr]).then(
+  result => console.log(result)
 ).catch(
-  reason => console.log('reason: ' + reason)
+  reason => console.log(reason)
 );
