466c6165023b8e752eceebf93fd313d0c6a659c2
Vadym Myrgorod <dealancer@gmail.com>
2020-02-24 21:17:14 -0500

    Refactor

--- flockleader.js
+++ flockleader.js
@@ -7,23 +7,17 @@ class FlockLeader {
     this.workerPool = new Array();
     this.promiseMap = new Map();
 
-    var onMessage = ({ command, workerId, funcId, func, args, result, reason }) => {
+    let onMessage = ({ command, workerId, funcId, func, args, result, reason }) => {
       if (command == 'done') {
-        const promise = this.promiseMap.get(funcId);
-        promise.resolve({ funcId: funcId, result: result });
+        this.promiseMap.get(funcId).resolve({ funcId: funcId, result: result });
       } else if (command == 'error') {
-        const promise = this.promiseMap.get(funcId);
-        promise.reject({ funcId: funcId, reason: reason });
+        this.promiseMap.get(funcId).reject({ funcId: funcId, reason: reason });
       } else if (command == 'run') {
-        this.runFunc({ funcId: funcId, func: func, args: args }).then(
-          ({ funcId, result }) => {
+        this.runFunc({ funcId: funcId, func: func, args: args }).then(({ funcId, result }) => {
             this.workerPool[workerId].send({ command: 'done', funcId: funcId, result: result })
-          }
-        ).catch(
-          ({ funcId, reason }) => {
-            this.workerPool[workerId].send({ command: 'error', funcId: funcId, reason: reason })
-          }
-        );
+        }).catch(({ funcId, reason }) => {
+          this.workerPool[workerId].send({ command: 'error', funcId: funcId, reason: reason })
+        });
       }
     }
 
@@ -38,16 +32,12 @@ class FlockLeader {
   }
 
   runFunc = function({ funcId, func, args }) {
-    if (!funcId) {
-      funcId = util.createId();
-    }
-    const promise = new Promise((resolve, reject) => {
+    funcId = funcId ? funcId : util.createId();
+    let promise = new Promise((resolve, reject) => {
       this.promiseMap.set(funcId, { resolve: resolve, reject: reject })
     });
-    var workerId = Math.floor(Math.random() * this.workerCount);
-    this.workerPool[workerId].send(
-      { command: 'run', funcId: funcId, func: func.toString(), args: args }
-    );
+    let workerId = Math.floor(Math.random() * this.workerCount);
+    this.workerPool[workerId].send({ command: 'run', funcId: funcId, func: func.toString(), args: args });
     return promise;
   }
 
--- worker.js
+++ worker.js
@@ -1,25 +1,23 @@
 const util = require('./util.js');
-
 const promiseMap = new Map();
 
 process.on('message', ({ command, funcId, func, args, result, reason }) => {
   if (command == 'done') {
-    const promise = promiseMap.get(funcId);
-    promise.resolve(result);
+    promiseMap.get(funcId).resolve(result);
   } else if (command == 'error') {
-    const promise = promiseMap.get(funcId);
-    promise.reject(reason);
+    promiseMap.get(funcId).reject(reason);
   } else if (command == 'run') {
-    var funcStr = func;
     eval("func = " + func);
-    const run = function(...args) {
-      const newFuncId = util.createId();
-      const promise = new Promise((resolve, reject) => {
+
+    let run = function(...args) {
+      let newFuncId = util.createId();
+      let promise = new Promise((resolve, reject) => {
         promiseMap.set(newFuncId, { resolve: resolve, reject: reject })
       });
-      process.send({ command: 'run', funcId: newFuncId, func: funcStr, args: args });
+      process.send({ command: 'run', funcId: newFuncId, func: func.toString(), args: args });
       return promise;
     };
+  
     func(...args).then(result => {
       process.send({ command: 'done', funcId: funcId, result: result });
     }).catch(reason => {
